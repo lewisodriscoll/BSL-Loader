993a2938b20964a60f0bafd1bc90f3bb150b9799
Lewis O'Driscoll <lewis.o'driscoll@stfc.ac.uk>
2016-07-25 12:16:29 +0100

    Add some comments

--- c_ext/bsl_loader.c
+++ c_ext/bsl_loader.c
@@ -6,6 +6,7 @@
 void loader_dealloc(Loader_params *params) {
     int i;
 
+    // Free the memory allocated in loader_init
     for (i = 0; i < params->rasters; i++) {
         free(params->data[i]);
     }
@@ -15,6 +16,7 @@ void loader_dealloc(Loader_params *params) {
 void loader_init(Loader_params *params) {
     int i;
 
+    // Allocate memory for a 2d array of size pixels*rasters
     params->data = malloc(sizeof(float *) * params->rasters);
     for (i = 0; i < params->rasters; i++) {
         params->data[i] = malloc(sizeof(float) * params->pixels);
@@ -22,6 +24,7 @@ void loader_init(Loader_params *params) {
 }
 
 void load_frame(Loader_params *params) {
+    // Struct for storing values read from file
     struct record {
         float x;
     };
@@ -40,8 +43,11 @@ void load_frame(Loader_params *params) {
         return;
     }
 
+    // Index of byte where the requried frame starts
     frame_pos = params->rasters * params->pixels * params->frame;
+    // Move the pointer to the start of the frame we're interested in
     fseek(input_file, frame_pos*sizeof(struct record), SEEK_SET);
+    // Read the frame data into params->data
     for (raster = 0; raster < params->rasters; raster++) {
         for (pixel = 0; pixel < params->pixels; pixel++) {
             fread(&my_record, sizeof(struct record), 1, input_file);
--- c_ext/try_loader.c
+++ c_ext/try_loader.c
@@ -2,18 +2,23 @@
 #include <string.h>
 
 int main() {
+    // Header file will be parsed in python.
+    // Pixels, raters, frame and filename will be passed in from python code.
     Loader_params params;
     params.pixels = 1475;
     params.rasters = 1679;
     params.frame = 5;
     strcpy(params.filename, "../multi_data/B12001.629");
 
+    // Allocates the params.data 2D array
     loader_init(&params);
 
+    // Loads the data from params.frame into params.data
     load_frame(&params);
 
     printf("data[766][1358] = %f\n", params.data[766][1358]);
 
+    // Frees the memory allocated in loader_init
     loader_dealloc(&params);
 
     return 1;
